8c3ef04d1ff8423dd28412f5bbf67abf4e22c04c
SamMaxwell <maxwellsamjm@gmail.com>
2022-02-23 16:29:18 +0100

    initialisation repo flask app

---
+++ API/API.py
@@ -0,0 +1,6 @@
+"""
+File: API.py
+Description: API for requesting hubEAU
+Author: Sam Maxwell
+Date: 02/2022
+"""
---
+++ Interface/Flaskr/app.py
@@ -0,0 +1,7 @@
+"""
+File: app.py
+Path: ./Interface/Flaskr/app.py
+Description: Flask APP for hydrological data visualisation (data from hubEAU)
+Author: Sam Maxwell
+Date: 02/2022
+"""
